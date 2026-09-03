0b2dc1599f9b0ba7611032c27c996235329a2501
Joshua Omotosho <omotoshojtsen2020@futa.edu.ng>
2026-09-03 05:41:55 +0100

    fix: update ts-jest config to use transform instead of deprecated globals

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,10 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-    preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/tests/**/*.test.ts'],
-    globals: {
-        'ts-jest': {
-            tsconfig: 'tsconfig.test.json',
-        },
+    transform: {
+        '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
     },
 };
 
